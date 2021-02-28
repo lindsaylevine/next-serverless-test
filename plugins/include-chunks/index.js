86b40478746742384718590e8ed95dd543bfd25c
Lindsay Levine <lindslev.ll@gmail.com>
2021-02-27 23:19:44 -0500

    uhh

--- plugins/include-chunks/index.js
+++ plugins/include-chunks/index.js
@@ -2,16 +2,20 @@ const fs = require("fs")
 const path = require("path")
 const AdmZip = require("adm-zip")
 const cpy = require("cpy")
+const copyFile = require("cp-file")
 // const config = require("../../next.config.js")
 
 module.exports = {
-  onBuild: async ({ constants: { FUNCTIONS_DIST } }) => {
+  onPostBuild: async ({ constants: { FUNCTIONS_DIST } }) => {
     const chunks = fs.readdirSync("./.next/serverless");
     console.log('CHUNKS', typeof chunks)
     Object.keys(chunks).forEach(async (i) => {
       const chunk = chunks[i]
-      console.log('????', FUNCTIONS_DIST)
-      await cpy(path.join('.', '.next/serverless', chunk), path.join(FUNCTIONS_DIST, chunk), { dot: true })
+      console.log('????', chunk, FUNCTIONS_DIST)
+      console.log(path.resolve(FUNCTIONS_DIST, chunk))
+      if (chunk.includes('header') || chunk.includes('footer')) {
+        await copyFile(path.resolve('.', '.next/serverless', chunk), path.resolve(FUNCTIONS_DIST, chunk))
+      }
       // const zipName = path.join(FUNCTIONS_DIST, chunk + ".zip")
       // const zip = new AdmZip(zipName)
       // const chunkPath = path.join('.', '.next/serverless', chunk)
